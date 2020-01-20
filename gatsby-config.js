794841ecbf8555203aa750c9198d3c2cc5c35b13
Vimalraj Selvam <email2vimalraj@gmail.com>
2020-01-20 22:06:05 +0800

    Added the gatsby-plugin-offline plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     'gatsby-plugin-sharp',
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-offline`,
     {
       resolve: 'gatsby-plugin-mdx',
       options: {
